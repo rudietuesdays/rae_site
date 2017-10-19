2f0ca6b6f80bca636ebb40e88ca2711824936160
Rae <amanda.gaines@gmail.com>
2017-10-18 23:28:37 -0400

    added portfolio page

--- client/assets/app.js
+++ client/assets/app.js
@@ -2,7 +2,8 @@ var app = angular.module('app', ['ngRoute', 'ngCookies', 'pdf']);
 app.config(function ($routeProvider) {
 	$routeProvider
 	.when('/', { templateUrl: 'partials/welcome.html'})
-  .when('/rae_resume', { templateUrl: 'partials/resume.html'})
+	.when('/rae_resume', { templateUrl: 'partials/resume.html'})
+  .when('/portfolio', { templateUrl: 'partials/portfolio.html'})
 	.otherwise({
     redirectTo: '/'
   });
